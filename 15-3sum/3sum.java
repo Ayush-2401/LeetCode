db1f8c4ad22a3568afc926801128b58402846cd0
Ayush Kumar Pandey <2401akpandeyxi@gmail.com>
2026-07-12 19:15:45 +0530

    Time: 30 ms (88.68%) | Memory: 53.8 MB (99.51%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,45 +1,45 @@
 class Solution {
-    // static int[] mergeSort(int[] arr){
-    //     if(arr.length == 1){
-    //         return arr;
-    //     }
+    static int[] mergeSort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
         
-    //     int mid = arr.length/2;
-    //     int[] left = mergeSort(Arrays.copyOfRange(arr,0,mid));
-    //     int[] right = mergeSort(Arrays.copyOfRange(arr, mid,arr.length));
-    //     return merge(left, right);
-    // }
-    // private static int[] merge(int[] f, int[] s){
-    //     int[] mix = new int[f.length+s.length];
-    //     int i = 0;
-    //     int j = 0;
-    //     int k = 0;
-    //     while(i<f.length && j<s.length){
-    //         if(f[i]<s[j]){
-    //             mix[k] = f[i];
-    //             i++;
-    //         }else{
-    //             mix[k] = s[j];
-    //             j++;
-    //         }
-    //         k++;
-    //     }
-    //     while(i<f.length){
-    //         mix[k]=f[i];
-    //         i++;
-    //         k++;
-    //     }
-    //     while(j<s.length){
-    //         mix[k]=s[j];
-    //         j++;
-    //         k++;
-    //     }
-    //     return mix;
-    // }
+        int mid = arr.length/2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid,arr.length));
+        return merge(left, right);
+    }
+    private static int[] merge(int[] f, int[] s){
+        int[] mix = new int[f.length+s.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i<f.length && j<s.length){
+            if(f[i]<s[j]){
+                mix[k] = f[i];
+                i++;
+            }else{
+                mix[k] = s[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<f.length){
+            mix[k]=f[i];
+            i++;
+            k++;
+        }
+        while(j<s.length){
+            mix[k]=s[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
 
     public List<List<Integer>> threeSum(int[] nums) {
        
-        Arrays.sort(nums);
+        nums = mergeSort(nums);
         List<List<Integer>> ans = new ArrayList<>(); 
         for(int i = 0; i<nums.length-2; i++){
             if(i>0 && nums[i] == nums[i-1]){
