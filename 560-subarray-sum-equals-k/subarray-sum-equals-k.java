081130e2de972e00ea228e67e07b52802f6481d1
Ayush Kumar Pandey <2401akpandeyxi@gmail.com>
2026-08-01 14:28:07 +0530

    Time: 23 ms (93.53%) | Memory: 48.8 MB (46.43%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,19 +1,34 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        for(int i = 0; i<nums.length; i++){
-            int j = i;
-            int sum = k;
-            while(j<nums.length){
+        // int count = 0;
+        // for(int i = 0; i<nums.length; i++){
+        //     int j = i;
+        //     int sum = k;
+        //     while(j<nums.length){
                 
-                sum = sum - nums[j]; 
+        //         sum = sum - nums[j]; 
                
-                if(sum == 0){
-                    count++;
+        //         if(sum == 0){
+        //             count++;
                     
-                }
-                j++;
+        //         }
+        //         j++;
+        //     }
+        // }
+        // return count;
+
+
+        int prefixSum = 0;
+        int count = 0;
+        HashMap<Integer, Integer> preffixfrequency = new HashMap<>();
+
+        preffixfrequency.put(0,1);
+        for(int i : nums){
+            prefixSum += i;
+            if(preffixfrequency.containsKey(prefixSum - k)){
+                count += preffixfrequency.get(prefixSum - k);
             }
+            preffixfrequency.put(prefixSum, preffixfrequency.getOrDefault(prefixSum, 0)+1);
         }
         return count;
     }
