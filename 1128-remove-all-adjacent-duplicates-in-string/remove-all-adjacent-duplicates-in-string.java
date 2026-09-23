62412fe6f4999bf43bc6b306e2d2249c4880d5e3
Ayush Kumar Pandey <2401akpandeyxi@gmail.com>
2026-09-23 22:19:36 +0530

    Time: 19 ms (69.62%) | Memory: 47.6 MB (13.70%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -2,22 +2,21 @@ class Solution {
     public String removeDuplicates(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         
-        for(int i = s.length()-1; i>=0; i--){
-            if(stack.isEmpty()){
-                stack.push(s.charAt(i));
-                continue;
-            } 
-            if(stack.peek()!=s.charAt(i)){
-                stack.push(s.charAt(i));
-            }else{
+        for(int i = 0; i<s.length(); i++){
+           
+            if(!stack.isEmpty() && stack.peek() == s.charAt(i)){
                 stack.pop();
+            }else{
+                stack.push(s.charAt(i));
             }
         }  
-        String ans = "";
-        while(!stack.isEmpty()){
-            ans += (stack.peek()).toString();
-            stack.pop();
-        } 
-        return ans;
+        StringBuilder ans = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            ans.append(stack.removeLast());
+        }
+
+        return ans.toString();
+        
     }
 }
